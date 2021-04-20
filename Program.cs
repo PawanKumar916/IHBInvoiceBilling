148330fa55f107b49d877e0b002a2f2fccc65696
PawanKumar916 <vermapawan916@gmail.com>
2021-04-20 14:30:58 +0530

    20042021Totday

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPatternExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Parallel.Invoke(
+                () => PrintStudentDetails(),
+                () => PrintEmployeeDetails()
+                );
+
+            Console.ReadKey();
+        }
+
+        private static void PrintStudentDetails()
+        {
+            Singleton fromEmployee = Singleton.GetInstance;
+            fromEmployee.PrintDetails("From Employee");
+        }
+        private static void PrintEmployeeDetails()
+        {
+            Singleton fromStudent = Singleton.GetInstance;
+            fromStudent.PrintDetails("From Student");
+        }
+    }
+}
---
+++ Singleton.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPatternExample
+{
+    public sealed class Singleton
+    {
+        private static int count = 0;
+        //private static readonly object obj = new object();
+        //private static Singleton instance = null;
+
+        //Eger Loading
+        //private static readonly Singleton instance = new Singleton();
+
+        //Lazy Loading
+        private static readonly Lazy<Singleton> instance = new Lazy<Singleton>(() => new Singleton());
+        public static Singleton GetInstance
+        {
+            get
+            {
+                //if (instance == null)
+                //{
+                //    lock (obj)
+                //    {
+                //        if (instance == null)
+                //        {
+                //            instance = new Singleton();
+                //        }
+                //    }
+                //}
+                //return instance;
+                return instance.Value;
+            }
+        }
+        private Singleton()
+        {
+            count++;
+            Console.WriteLine("Singleton Counter Value is : " + count.ToString());
+        }
+        public void PrintDetails(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+
+}
